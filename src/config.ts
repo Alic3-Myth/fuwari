6adb6bdbf9f0acafe5b99ad3ee61e6d4911e6f1f
Alic3-Myth <2279742036@qq.com>
2024-12-22 01:55:33 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -43,6 +43,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
+      name: 'Friends',
+      url: '/friends/',     // Internal links should not include the base path, as it is automatically added
+      //external: true,                               // Show an external link icon and will open in a new tab
+    },    
+    {
       name: 'GitHub',
       url: 'https://github.com/Alic3-Myth',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
